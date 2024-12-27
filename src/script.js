dc056279d8e44cb0aa390e7afd029764f18f2b21
abuTalha221 <abutalhajr1236@gmail.com>
2024-12-27 09:04:44 +0600

    Final

--- src/script.js
+++ src/script.js
@@ -1,10 +1,98 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     const buttons = document.querySelectorAll('#button');
-    const displayInput = document.querySelectorAll('#display-input"');
-    const displayOutput = document.querySelectorAll('#display-output');
+    const displayInput = document.querySelector('#display-input');
+    const displayOutput = document.querySelector('#display-output');
+
+    const input = [];
+    let isParenthesesOpen = false;
+
+    function clearInput() {
+        input.length = 0;
+        displayInput.textContent = ' ';
+    }
+
+    function clearOutput() {
+        displayOutput.textContent = ' ';
+    }
+
+    function clear() {
+        clearInput();
+        clearOutput();
+    }
+
+    function deletelast() {
+        input.pop();
+        updateDisplayInput();
+    }
+
+    function calculate() {
+        const result = eval(input.join(''));
+        displayOutput.textContent = `= ${result}`;
+    }
+
+    function updateDisplayInput() {
+        displayInput.textContent = input.join('');
+    }
+
+    function updateInput(value) {
+        const map = new Map();
+        map.set('divide', '/');
+        map.set('multiply', '*');
+        map.set('add', '+');
+        map.set('subtract', '-');
+        map.set('dot', '.');
+
+        if (map.has(value)) {
+            input.push(map.get(value));
+        }
+        else {
+            input.push(value);
+        }
+        updateDisplayInput();
+    }
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+
+            const value = button.dataset.value;
+
+            switch (value) {
+                case 'clear':
+                    clear();
+                    break;
+
+                case 'delete':
+                    deletelast();
+                    clearOutput();
+                    break;
+
+                case 'equal':
+                    calculate();
+                    break;
+
+                case 'parentheses':
+                    if (isParenthesesOpen) {
+                        input.push(')');
+                        isParenthesesOpen = false;
+                        updateDisplayInput();
+                    }
+                    else {
+                        input.push('(');
+                        isParenthesesOpen = true;
+                        updateDisplayInput();
+                    }
+
+                    break;
+
+                default:
+                    updateInput(value);
+                    break;
+
+            }
 
 
-
+        });
+    });
 
 });
